a867d2160d7656774591d1acada3f6734899550b
Forrest Guice <forrestguice@users.noreply.github.com>
2019-01-12 01:40:47 -0700

    database
    add fields

--- app/src/main/java/com/forrestguice/topoindex/database/TopoIndexDatabaseAdapter.java
+++ app/src/main/java/com/forrestguice/topoindex/database/TopoIndexDatabaseAdapter.java
@@ -33,16 +33,64 @@ public class TopoIndexDatabaseAdapter
     public static final String KEY_ROWID = "_id";
     public static final String DEF_ROWID = KEY_ROWID + " integer primary key autoincrement";
 
-    public static final String KEY_QUAD_NAME = "name";
-    public static final String DEF_QUAD_NAME = KEY_QUAD_NAME + " text not null";
+    public static final String KEY_MAP_SERIES = "series";
+    public static final String DEF_MAP_SERIES = KEY_MAP_SERIES + " text not null";
 
-    private static final String TABLE_QUADS = "quads";
-    private static final String TABLE_QUADS_CREATE_COLS = DEF_ROWID + ", "
-            + DEF_QUAD_NAME;
-    private static final String TABLE_QUADS_CREATE = "create table " + TABLE_QUADS + " (" + TABLE_QUADS_CREATE_COLS + ");";
+    public static final String KEY_MAP_VERSION = "version";
+    public static final String DEF_MAP_VERSION = KEY_MAP_VERSION + " text not null";
 
-    private static final String[] QUERY_QUADS_MINENTRY = new String[] {KEY_ROWID, KEY_QUAD_NAME};
-    private static final String[] QUERY_QUADS_FULLENTRY = new String[] {KEY_ROWID, KEY_QUAD_NAME};
+    public static final String KEY_MAP_CELLID = "cellid";
+    public static final String DEF_MAP_CELLID = KEY_MAP_CELLID + " text not null";
+
+    public static final String KEY_MAP_NAME = "name";
+    public static final String DEF_MAP_NAME = KEY_MAP_NAME + " text not null";
+
+    public static final String KEY_MAP_STATE = "state";
+    public static final String DEF_MAP_STATE = KEY_MAP_STATE + " text not null";
+
+    public static final String KEY_MAP_DATE = "mapdate";
+    public static final String DEF_MAP_DATE = KEY_MAP_STATE + " text not null";
+
+    public static final String KEY_MAP_LATITUDE_NORTH = "nlat";
+    public static final String DEF_MAP_LATITUDE_NORTH = KEY_MAP_LATITUDE_NORTH + " text not null";
+
+    public static final String KEY_MAP_LONGITUDE_WEST = "wlon";
+    public static final String DEF_MAP_LONGITUDE_WEST = KEY_MAP_LONGITUDE_WEST + " text not null";
+
+    public static final String KEY_MAP_LATITUDE_SOUTH = "slat";
+    public static final String DEF_MAP_LATITUDE_SOUTH = KEY_MAP_LATITUDE_SOUTH + " text not null";
+
+    public static final String KEY_MAP_LONGITUDE_EAST = "elon";
+    public static final String DEF_MAP_LONGITUDE_EAST = KEY_MAP_LONGITUDE_EAST + " text not null";
+
+    public static final String KEY_MAP_SCALE = "scale";
+    public static final String DEF_MAP_SCALE = KEY_MAP_SCALE + " text not null";
+
+    public static final String KEY_MAP_DATUM = "datum";
+    public static final String DEF_MAP_DATUM = KEY_MAP_DATUM + " text not null";
+
+    public static final String KEY_MAP_PROJECTION = "projection";
+    public static final String DEF_MAP_PROJECTION = KEY_MAP_PROJECTION + " text not null";
+
+    private static final String TABLE_MAPS = "maps";
+    private static final String TABLE_MAPS_CREATE_COLS = DEF_ROWID + ", "
+            + DEF_MAP_SERIES + ", "
+            + DEF_MAP_VERSION + ", "
+            + DEF_MAP_CELLID + ", "
+            + DEF_MAP_NAME + ", "
+            + DEF_MAP_STATE + ", "
+            + DEF_MAP_SCALE + ", "
+            + DEF_MAP_DATE + ", "
+            + DEF_MAP_DATUM + ", "
+            + DEF_MAP_PROJECTION + ", "
+            + DEF_MAP_LATITUDE_NORTH + ", "
+            + DEF_MAP_LONGITUDE_WEST + ", "
+            + DEF_MAP_LATITUDE_SOUTH + ", "
+            + DEF_MAP_LONGITUDE_EAST;
+    private static final String TABLE_MAPS_CREATE = "create table " + TABLE_MAPS + " (" + TABLE_MAPS_CREATE_COLS + ");";
+
+    private static final String[] QUERY_MAPS_MINENTRY = new String[] {KEY_ROWID, KEY_MAP_SERIES, KEY_MAP_VERSION, KEY_MAP_CELLID, KEY_MAP_NAME, KEY_MAP_DATE, KEY_MAP_STATE};
+    private static final String[] QUERY_MAPS_FULLENTRY = new String[] {KEY_ROWID, KEY_MAP_SERIES, KEY_MAP_VERSION, KEY_MAP_CELLID, KEY_MAP_NAME, KEY_MAP_DATE, KEY_MAP_STATE, KEY_MAP_SCALE, KEY_MAP_DATUM, KEY_MAP_PROJECTION, KEY_MAP_LATITUDE_NORTH, KEY_MAP_LONGITUDE_WEST, KEY_MAP_LATITUDE_SOUTH, KEY_MAP_LONGITUDE_EAST};
 
     /**
      *
@@ -75,9 +123,9 @@ public class TopoIndexDatabaseAdapter
 
     public Cursor getAllQuads(int n, boolean fullEntry)
     {
-        String[] QUERY = (fullEntry) ? QUERY_QUADS_FULLENTRY : QUERY_QUADS_MINENTRY;
-        Cursor cursor =  (n > 0) ? database.query( TABLE_QUADS, QUERY, null, null, null, null, "_id DESC", n+"" )
-                                 : database.query( TABLE_QUADS, QUERY, null, null, null, null, "_id DESC" );
+        String[] QUERY = (fullEntry) ? QUERY_MAPS_FULLENTRY : QUERY_MAPS_MINENTRY;
+        Cursor cursor =  (n > 0) ? database.query( TABLE_MAPS, QUERY, null, null, null, null, "_id DESC", n+"" )
+                                 : database.query( TABLE_MAPS, QUERY, null, null, null, null, "_id DESC" );
         if (cursor != null)
         {
             cursor.moveToFirst();
@@ -88,8 +136,8 @@ public class TopoIndexDatabaseAdapter
     public Cursor getQuad(long row) throws SQLException
     {
         @SuppressWarnings("UnnecessaryLocalVariable")
-        String[] QUERY = QUERY_QUADS_FULLENTRY;
-        Cursor cursor = database.query( true, TABLE_QUADS, QUERY,
+        String[] QUERY = QUERY_MAPS_FULLENTRY;
+        Cursor cursor = database.query( true, TABLE_MAPS, QUERY,
                 KEY_ROWID + "=" + row, null,
                 null, null, null, null );
         if (cursor != null)
@@ -117,7 +165,7 @@ public class TopoIndexDatabaseAdapter
                 //noinspection ConstantConditions
                 case 0:
                 default:
-                    db.execSQL(TABLE_QUADS_CREATE);
+                    db.execSQL(TABLE_MAPS_CREATE);
                     break;
             }
         }
